2fcd1bbe7130ef52235618c229886cb52c7d7fb2
Rui-Ferreira3 <80005981+Rui-Ferreira3@users.noreply.github.com>
2024-04-10 18:31:09 +0100

    Delete src directory

--- src/defines.h
+++
@@ -1,9 +0,0 @@
-#ifndef DEFINES_H
-#define DEFINES_H
-
-#define ACCELERATOR_BASE_ADDRESS            0x90000000
-#define MEM_BASE_ADDRESS                    0x02000000
-
-#define NUM_MATMULS 100
-
-#endif //DEFINES_H
